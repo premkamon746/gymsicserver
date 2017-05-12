035e703f6a6c60add3b553d4b99f2812c0ee2d9d
premkamon-PC\premkamon <premkamon746@gmail.com>
2017-05-13 00:33:21 +0700

    mongo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var monk = require('monk');
+var db = monk('localhost:27017/gymsic');
+var collection = db.get('music');
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/', function(req, res, next) {
+  var rz =   collection.find({}, function(err, music){
+		if (err) res.json(500, err);
+		else res.json(music);
+	});
+
 });
 
 module.exports = router;
