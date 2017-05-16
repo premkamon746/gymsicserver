0c6f80425a1db035e62d9b29b476841ce62b1810
premkamon-PC\premkamon <premkamon746@gmail.com>
2017-05-16 19:33:57 +0700

    change search option

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,23 @@ var monk = require('monk');
 var db = monk('localhost:27017/gymsic');
 var collection = db.get('music');
 
+
 /* GET home page. */
-router.post('/', function(req, res, next) {
+router.get('/', function(req, res, next) {
+  console.log(req.body.search);
   var rz =   collection.find({}, function(err, music){
+		if (err) res.json(500, err);
+		else res.json(music);
+	});
+
+});
+
+router.post('/', function(req, res, next) {
+  //console.log(req.body);
+  var search = req.body.search;
+  var rz =   collection.find({ $or : [ { "title" : new RegExp(search, 'i') }, {"artist" : new RegExp(search, 'i')} ] } 
+
+  	, function(err, music){
 		if (err) res.json(500, err);
 		else res.json(music);
 	});
